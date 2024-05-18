b18db67f9b4f3b3ba5b4a874c769b1f786fd1b38
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-05-18 13:45:25 +0545

    feat: setup endpoint

--- apps/api/core/views.py
+++ apps/api/core/views.py
@@ -10,6 +10,7 @@ from rest_framework.viewsets import GenericViewSet
 from .models import User
 from .models import Workspace
 from .serializers import GenericOut
+from .serializers import NeedSetupOut
 from .serializers import UserLoginIn
 from .serializers import UserOut
 from .serializers import UserRegisterIn
@@ -29,10 +30,10 @@ class UserViewSet(GenericViewSet):
                 {"detail": "Admin already exists."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        user = User.objects.create_user(
-            username=serializer.validated_data["username"],
-            email=serializer.validated_data["email"],
-            password=serializer.validated_data["password"],
+        user = User.objects.create_user(  # type: ignore
+            username=serializer.validated_data["username"],  # type: ignore
+            email=serializer.validated_data["email"],  # type: ignore
+            password=serializer.validated_data["password"],  # type: ignore
             role="ADMIN",
         )
         default_workspace, _ = Workspace.objects.get_or_create(
@@ -50,8 +51,8 @@ class UserViewSet(GenericViewSet):
     def login(self, request: Request):
         serializer = UserLoginIn(data=request.data)
         serializer.is_valid(raise_exception=True)
-        username = serializer.validated_data["username"]
-        password = serializer.validated_data["password"]
+        username = serializer.validated_data["username"]  # type: ignore
+        password = serializer.validated_data["password"]  # type: ignore
 
         user = authenticate(request, username=username, password=password)
         if user is None:
@@ -60,7 +61,22 @@ class UserViewSet(GenericViewSet):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        login(request, user)
+        login(request, user)  # type: ignore
         return Response(
             {"user": UserOut(user).data}, status=status.HTTP_200_OK
+        )
+
+    @extend_schema(responses=NeedSetupOut)
+    @action(detail=False, methods=["get"], url_path="setup")
+    def need_setup(self, request: Request):
+        admin_user_exists = User.objects.filter(role="ADMIN").exists()
+        if admin_user_exists:
+            return Response(
+                {"need_setup": False},
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            {"need_setup": True},
+            status=status.HTTP_200_OK,
         )

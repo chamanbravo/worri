b10fa0c1382391d5c722161d2640679bc7d85673
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-09-25 20:37:20 +0545

    chore: remove drf apis

--- apps/api/worri_api/__init__.py
+++
@@ -1,5 +0,0 @@
-import django_stubs_ext
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.viewsets import ModelViewSet
-
-django_stubs_ext.monkeypatch(extra_classes=(ModelViewSet, GenericViewSet))
